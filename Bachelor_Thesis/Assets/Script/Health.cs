841b37ad96d13071db24865db6de137fc4ff060e
Quentin <quentin.flueckiger@hispeed.ch>
2018-12-12 14:07:07 +0100

    Local network

---
+++ Bachelor_Thesis/Assets/Script/Health.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class Health : NetworkBehaviour {
+
+    public RectTransform healthBar;
+    public const int maxHealth = 100;
+    public bool destroyOnDeath;
+
+    [SyncVar (hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHealth = maxHealth;
+                RpcRespawn();
+            }
+        }
+
+    }
+
+    void OnChangeHealth(int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+
+        if (isLocalPlayer)
+        {
+            // move back to zero location
+            transform.position = Vector3.zero;
+        }
+    }
+}
---
+++ Bachelor_Thesis/Assets/Script/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+        
+        transform.Translate(x, y, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * 6;
+
+        // Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
