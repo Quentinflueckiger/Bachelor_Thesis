43172aac533546b05eed3154a1c1d821208bb6e5
Quentin <quentin.flueckiger@hispeed.ch>
2019-03-05 17:35:53 +0100

    UNET try out

---
+++ Unet_Udemy/CameraFollow/Assets/SetupLocalPlayer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+    public Text namePrefab;
+    public Text nameLabel;
+    public Transform namePos;
+
+    private string textBoxName = "";
+
+    [SyncVar (hook = "OnChangeName")]
+    public string pName = "player";
+
+	void Start () 
+	{
+		if(isLocalPlayer)
+		{
+			GetComponent<PlayerController>().enabled = true;
+            CameraFollow360.player = this.gameObject.transform;
+		}
+		else
+		{
+			GetComponent<PlayerController>().enabled = false;
+		}
+
+        GameObject canvas = GameObject.FindWithTag("MainCanvas");
+        nameLabel = Instantiate(namePrefab, Vector3.zero, Quaternion.identity) as Text;
+        nameLabel.transform.SetParent(canvas.transform); 
+	}
+
+	void Update()
+	{
+        Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
+        nameLabel.transform.position = nameLabelPos;
+	}
+
+    private void OnGUI()
+    {
+        if (isLocalPlayer)
+        {
+            textBoxName = GUI.TextField(new Rect(25, 15, 100, 25), textBoxName);
+            if (GUI.Button(new Rect(130, 15, 35, 25), "Set"))
+                CmdChangeName(textBoxName);
+        }
+    }
+
+    [Command]
+    private void CmdChangeName(string newName)
+    {
+        pName = newName;
+        nameLabel.text = pName;
+    }
+
+    private void OnChangeName(string newName)
+    {
+        pName = newName;
+        nameLabel.text = pName;
+    }
+}
---
+++ Unet_Udemy/TheNetworkManager1/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿//adapted from example script available at
+//https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+	public float speed = 25.0F;
+    public float rotationSpeed = 50.0f;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+	
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        Quaternion turn = Quaternion.Euler(0f, rotation, 0f);
+        rb.MovePosition(rb.position + this.transform.forward * translation);
+        rb.MoveRotation(rb.rotation*turn);
+	}
+}
