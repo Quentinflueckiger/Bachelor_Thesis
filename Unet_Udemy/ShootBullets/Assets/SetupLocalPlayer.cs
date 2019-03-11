97323821fe368631c817cfdb9ed90a304e9faa38
Quentinflueckiger <quentin.flueckiger@hispeed.ch>
2019-03-12 00:04:42 +0100

    Udemy Unet
    -Finished tank
    -Started section 7, customised network manager

--- Unet_Udemy/ShootBullets/Assets/SetupLocalPlayer.cs
+++ Unet_Udemy/ShootBullets/Assets/SetupLocalPlayer.cs
@@ -8,14 +8,22 @@ public class SetupLocalPlayer : NetworkBehaviour {
 	public Text namePrefab;
 	public Text nameLabel;
 	public Transform namePos;
+    public Slider healthPrefab;
+    public Slider health;
+    public GameObject explosion;
+    NetworkStartPosition[] spawnPos;
 	string textboxname = "";
 	string colourboxname = "";
+
 
 	//[SyncVar (hook = "OnChangeName")]
 	public string pName = "player";
 
 	//[SyncVar (hook = "OnChangeColour")]
 	public string pColour = "#ffffff";
+
+    //[SyncVar (hook = "OnChangeHealth")]
+    public int healthValue = 100;
 
     // Syncvar not working since 07.03.2019
     // It makes the client freeze and get stuck that way
@@ -41,6 +49,23 @@ public class SetupLocalPlayer : NetworkBehaviour {
         }
     }
 
+    [ClientRpc]
+    private void RpcOnChangeHealth (int n)
+    {
+        healthValue = n;
+        health.value = healthValue;
+    }
+
+    [ClientRpc]
+    private void RpcRespawn()
+    {
+        if (!isLocalPlayer) return;
+        if (spawnPos != null && spawnPos.Length > 0)
+        {
+            this.transform.position = spawnPos[Random.Range(0, spawnPos.Length)].transform.position;
+        }
+    }
+
 	[Command]
 	public void CmdChangeName(string newName)
 	{
@@ -63,6 +88,25 @@ public class SetupLocalPlayer : NetworkBehaviour {
         RpcOnChangeColour(newColour);
 	}
 
+    [Command]
+    public void CmdChangeHealth(int hitValue)
+    {
+        healthValue = healthValue + hitValue;
+        health.value = healthValue;
+
+        if (health.value <= 0)
+        {
+            GameObject e = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            NetworkServer.Spawn(e);
+            this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            RpcRespawn();
+            healthValue = 100;
+        }
+
+        RpcOnChangeHealth(healthValue);
+
+    }
+
 	void OnGUI()
 	{
 		if(isLocalPlayer)
@@ -77,10 +121,17 @@ public class SetupLocalPlayer : NetworkBehaviour {
 		}
 	}
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isLocalPlayer && collision.gameObject.tag =="bullet")
+        {
+            CmdChangeHealth(-5);
+        }
+    }
 
-	//Credit for this method: from http://answers.unity3d.com/questions/812240/convert-hex-int-to-colorcolor32.html
-	//hex for testing green: 04BF3404  red: 9F121204  blue: 221E9004
-	Color ColorFromHex(string hex)
+    //Credit for this method: from http://answers.unity3d.com/questions/812240/convert-hex-int-to-colorcolor32.html
+    //hex for testing green: 04BF3404  red: 9F121204  blue: 221E9004
+    Color ColorFromHex(string hex)
 	{
 		hex = hex.Replace ("0x", "");
         hex = hex.Replace ("#", "");
@@ -111,16 +162,26 @@ public class SetupLocalPlayer : NetworkBehaviour {
 		GameObject canvas = GameObject.FindWithTag("MainCanvas");
 		nameLabel = Instantiate(namePrefab, Vector3.zero, Quaternion.identity) as Text;
 		nameLabel.transform.SetParent(canvas.transform);
+
+        health = Instantiate(healthPrefab, Vector3.zero, Quaternion.identity) as Slider;
+        health.transform.SetParent(canvas.transform);
+
+        spawnPos = FindObjectsOfType<NetworkStartPosition>();
 	}
+
     public override void OnStartClient()
     {
         base.OnStartClient();
         //Invoke("Init", 1);
     }
+
     public void OnDestroy()
 	{
-		if(nameLabel != null)
-			Destroy(nameLabel.gameObject);
+        if (nameLabel != null && health != null)
+        {
+            Destroy(nameLabel.gameObject);
+            Destroy(health.gameObject);
+        }
 	}
 
 	void Update()
@@ -131,15 +192,19 @@ public class SetupLocalPlayer : NetworkBehaviour {
 			Vector3 screenPoint = Camera.main.WorldToViewportPoint(this.transform.position);
 			bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && 
 			                screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
-			//if it is on screen draw its label attached to is name position
-			if(onScreen)
-			{
-				Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
-				nameLabel.transform.position = nameLabelPos;
-			}
-			else //otherwise draw it WAY off the screen 
-				nameLabel.transform.position = new Vector3(-1000,-1000,0);
-		}
+            //if it is on screen draw its label attached to is name position
+            if (onScreen)
+            {
+                Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
+                nameLabel.transform.position = nameLabelPos;
+                health.transform.position = nameLabelPos + new Vector3(0, 20, 0);
+            }
+            else //otherwise draw it WAY off the screen 
+            {
+                nameLabel.transform.position = new Vector3(-1000, -1000, 0);
+                health.transform.position = new Vector3(-1000, -1000, 0);
+            }
+        }
 	}
 
     /*private void Init()
